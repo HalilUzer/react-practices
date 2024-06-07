8a8d522ff16e50e4692f58a81a5a379cdd988e1b
Halil Uzer <haliluzer@outlook.com>
2024-06-07 12:48:26 +0300

    blog: delete modal updated

--- blog/src/DeletePostModal.jsx
+++ blog/src/DeletePostModal.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { createPortal } from 'react-dom'
-import { useDeletePostMutation } from './features/posts/postApi';
+import {createPortal} from 'react-dom'
+import {useDeletePostMutation} from './features/posts/postApi';
 import {useNavigate} from "react-router-dom";
 
 
-export default function DeletePostModal({ postId, isModalOpen, setIsModelOpen }) {
+export default function DeletePostModal({postId, isModalOpen, setIsModelOpen}) {
     const [deletePost] = useDeletePostMutation();
 
     const navigate = useNavigate();
@@ -12,18 +12,20 @@ export default function DeletePostModal({ postId, isModalOpen, setIsModelOpen })
     if (!isModalOpen) return null
     const handleDelete = (e) => {
         e.preventDefault();
-        deletePost({ id: postId });
+        deletePost({id: postId});
         setIsModelOpen(false);
         navigate('/');
     }
 
     return (
-        <div className='Modal'>
-            <h2>Are You Sure?</h2>
-            <form action="" onSubmit={(e) => e.preventDefault()}>
-                <button className='AcceptButton' onClick={handleDelete}>Yes</button>
-                <button className='DenyButton' onClick={e => setIsModelOpen(false)}>No</button>
-            </form>
+        <div className="ModalBackground">
+            <div className='Modal'>
+                <h2>Are You Sure?</h2>
+                <form action="" onSubmit={(e) => e.preventDefault()}>
+                    <button className='AcceptButton' onClick={handleDelete}>Yes</button>
+                    <button className='DenyButton' onClick={e => setIsModelOpen(false)}>No</button>
+                </form>
+            </div>
         </div>
     )
 }
