4cbd87dca35a91cd075d0f00bb6fc21f63da769a
Halil Uzer <haliluzer@outlook.com>
2024-06-12 18:19:09 +0300

    blog: tailwind update

--- blog/src/Header.jsx
+++ blog/src/Header.jsx
@@ -1,20 +1,22 @@
-import {FaLaptop, FaTabletAlt, FaMobileAlt} from "react-icons/fa";
+import { FaLaptop, FaTabletAlt, FaMobileAlt } from "react-icons/fa";
 import './index.css'
 import useWindowSize from "./hooks/useWindowSize.js";
 import ThemeToggleButton from "./ThemeToggleButton.jsx";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
 
-    const {width} = useWindowSize()
+    const { width } = useWindowSize()
+    const navigate = useNavigate()
 
     return (
-        <header className='Header'>
-            <h1>React.Js Blog</h1>
-            <div className='HeaderButtons'>
-                <ThemeToggleButton/>
-                {width < 768 ? <FaMobileAlt/> :
-                    width < 992 ? <FaTabletAlt/> :
-                        <FaLaptop/>}
+        <header className='flex justify-between items-center bg-[#66d8f5] size-20 p-6 w-full'>
+            <h1>React.Js Blog</h1> 
+            <div className={'flex w-1/3 justify-around items-center'}>
+                <ThemeToggleButton />
+                {width < 768 ? <FaMobileAlt /> :
+                    width < 992 ? <FaTabletAlt /> :
+                        <FaLaptop />}
             </div>
 
         </header>
--- blog/src/Nav.jsx
+++ blog/src/Nav.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-export default function Nav({keyword, setKeyword, type = ''}) {
+export default function Nav({ keyword, setKeyword, type = '' }) {
 
     return (
-        <nav className='flex space-x-0 bg-[#333] '>
+        <nav className='flex space-x-0 bg-[#333] text-white'>
             {type === 'search' && <form action="">
                 <label htmlFor="searchBar">Search Bar</label>
                 <input type="text" id="searchBar" placeholder='Search Posts' onChange={e => setKeyword(e.target.value)}
-                       value={keyword}/>
+                    value={keyword} />
+
             </form>}
-            <ul>
-                <li><Link to={"/"}>Home</Link></li>
-                <li><Link to={"/post"}>Posts</Link></li>
-                <li><Link to={"/about"}>About</Link></li>
+            <ul className={'flex justify-evenly list-none flex-grow text-2xl'}>
+                <li className={'m-4 shadow-[rgba(0, 0, 0, 0.06)_0_2px_4px] hover:shadow-[rgba(0, 0, 0, 0.06)_0_2px_4px transition-all'}><Link to={"/"}>Home</Link></li>
+                <li className={'m-4 shadow-[rgba(0, 0, 0, 0.06)_0_2px_4px] hover:shadow-[rgba(0, 0, 0, 0.06)_0_2px_4px transition-all'}><Link to={"/post"}>Posts</Link></li>
+                <li className={'m-4 shadow-[rgba(0, 0, 0, 0.06)_0_2px_4px] hover:shadow-[rgba(0, 0, 0, 0.06)_0_2px_4px transition-all'}><Link to={"/about"}>About</Link></li>
             </ul>
         </nav>
     )
