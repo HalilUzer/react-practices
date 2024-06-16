560e7e726f3eff516d23a404f6b83cacdadb19f7
Halil Uzer <haliluzer@outlook.com>
2024-06-16 19:16:13 +0300

    blog: toggle button updated

--- blog/src/components/ThemeToggleButton.jsx
+++ blog/src/components/ThemeToggleButton.jsx
@@ -7,11 +7,11 @@ export default function ThemeToggleButton() {
 
     return (
         <div className="">
-            <input type="checkbox" id="theme" className="peer h-0 w-0 hidden checked:translate-x-full"
+            <input type="checkbox" id="theme" className="peer h-0 w-0 hidden"
                 onClick={e => themeContext.setIsDark(!themeContext.isDark)} value={themeContext.isDark} />
             <label htmlFor="theme" className="flex relative before:bg-white before:w-20 h-8 before:cursor-pointer before:rounded-3xl before:justify-start before:items-center
             after:absolute after:w-5 after:h-5 after:bg-black after:rounded-3xl after:left-1 after:top-1.5
-            peer-checked:after:translate-x-full"></label>
+            peer-checked:after:translate-x-12"></label>
         </div>)
 }
 
