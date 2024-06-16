8ec6b924585e3626e8e952628f552675fd4e40d0
Halil Uzer <haliluzer@outlook.com>
2024-06-16 19:31:02 +0300

    blog: toggle button updated

--- blog/src/components/ThemeToggleButton.jsx
+++ blog/src/components/ThemeToggleButton.jsx
@@ -11,7 +11,7 @@ export default function ThemeToggleButton() {
                 onClick={e => themeContext.setIsDark(!themeContext.isDark)} value={themeContext.isDark} />
             <label htmlFor="theme" className="flex relative before:bg-white before:w-20 h-8 before:cursor-pointer before:rounded-3xl before:justify-start before:items-center
             after:absolute after:w-5 after:h-5 after:bg-black after:rounded-3xl after:left-1 after:top-1.5
-            peer-checked:after:translate-x-12"></label>
+            peer-checked:after:translate-x-12 after:transition-all"></label>
         </div>)
 }
 
