bb6f658e2ea7aac0d4c02a06e4dbd19c844d4404
Halil Uzer <haliluzer@outlook.com>
2024-06-30 11:14:30 +0300

    blog: sign-up updated

--- blog/src/routers/SignUpRouter.jsx
+++ blog/src/routers/SignUpRouter.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function SignUpRouter() {
     return (
-        <main className='flex justify-center items-center'>
-            <form action="">
-                <h2>Sign-In</h2>
+        <main className='flex justify-center items-center min-h-[100vh]'>
+            <form className='flex flex-col justify-center items-center min-h-[50vh]' action="">
+                <h2 className='mr-auto'>Sign-In</h2>
                 <label htmlFor="username">Username:</label>
                 <input type="text" id='username' required/>
                 <label htmlFor="password">Password:</label>
