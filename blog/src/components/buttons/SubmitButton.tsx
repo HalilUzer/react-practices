de84a54698378f618f511c100fb3b760c0e2a4ea
Halil Uzer <haliluzer@outlook.com>
2024-07-02 22:26:14 +0300

    blog: submit button migrated to ts

--- blog/src/components/buttons/SubmitButton.tsx
+++ blog/src/components/buttons/SubmitButton.tsx
@@ -3,13 +3,15 @@ import type { ReactNode, MouseEventHandler } from "react"
 
 type Props = {
     children: ReactNode,
-    onClick: MouseEventHandler<HTMLButtonElement>
+    onClick: MouseEventHandler<HTMLButtonElement>,
+    className?: string,
+    type?: 'button' | 'submit' | 'reset'
 }
 
-export default function SubmitButton({ children, onClick }: Props) {
+export default function SubmitButton({ children, onClick, className = '', type = 'submit' }: Props) {
 
     return (
-        <button onClick={onClick} type="submit" className="grid group bg-gray-100 border border-black border-solid rounded-lg mb-2 hover:bg-gray-300 place-content-center p-1 dark:bg-dark-blue dark:hover:bg-black" >
+        <button onClick={onClick} type={type} className={className.length === 0 ? "grid group bg-gray-100 border border-black border-solid rounded-lg mb-2 hover:bg-gray-300 place-content-center p-1 dark:bg-dark-blue dark:hover:bg-black" : className} >
             {children}
         </button>
     )
