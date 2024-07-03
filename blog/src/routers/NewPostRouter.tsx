eda001d893846fb8c6b7b2f99b40efc1d05accb5
Halil Uzer <haliluzer@outlook.com>
2024-07-03 17:52:50 +0300

    blog: newPostRouter updated

--- blog/src/routers/NewPostRouter.tsx
+++ blog/src/routers/NewPostRouter.tsx
@@ -2,7 +2,9 @@ import React, { useState } from 'react'
 import { NewPost, useAddPostMutation } from '../features/posts/postApi.ts'
 import { useNavigate } from 'react-router-dom';
 import { MouseEvent } from 'react';
-import PostEditForm from '../components/PostEditForm.tsx';
+import { MdOutlineDone } from 'react-icons/md';
+import { ClipLoader } from 'react-spinners';
+import Button from '../components/buttons/Button.tsx';
 
 
 
@@ -11,9 +13,10 @@ export default function NewPostRouter() {
     const [isLoading, setIsLoading] = useState(false);
     const [isDone, setIsDone] = useState(false)
     const [addPost] = useAddPostMutation()
+    const [newPost, setNewPost] = useState<NewPost>({ title: '', body: '' });
     const navigate = useNavigate()
 
-    const handleSubmit = async (e : MouseEvent<HTMLButtonElement>, newPost : NewPost) => {
+    const handleSubmit = async (e: MouseEvent<HTMLButtonElement>, newPost: NewPost) => {
         e.preventDefault();
         try {
             const addPostPromise = addPost(newPost)
@@ -27,6 +30,15 @@ export default function NewPostRouter() {
     }
 
     return (
-        <PostEditForm handleSubmit={handleSubmit} isDone={isDone} isLoading={isLoading} />
+        <form className='flex flex-col p-3' action="" onSubmit={e => e.preventDefault()}>
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" required onChange={e => setNewPost({ ...newPost, title: e.target.value })} autoFocus className="border border-black border-solid rounded-lg dark:bg-dark-blue " value={newPost.title} />
+            <label htmlFor="post">Post</label>
+            <textarea id="post" required rows={9} cols={10} onChange={e => setNewPost({ ...newPost, body: e.target.value })} className="border border-black border-solid rounded-lg mb-3 dark:bg-dark-blue" value={newPost.body} />
+            <Button onClick={e => handleSubmit(e, newPost!)}>
+                {isLoading ? <ClipLoader size={25} /> : isDone ?
+                    <MdOutlineDone size={25} /> : "Submit"}
+            </Button>
+        </form>
     )
 }
