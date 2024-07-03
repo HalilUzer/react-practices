e99f57bb879e8fddccb53aee3a3d365a9983485a
Halil Uzer <haliluzer@outlook.com>
2024-07-03 18:30:42 +0300

    blog: reusable components added to NewPostRouter

--- blog/src/routers/NewPostRouter.tsx
+++ blog/src/routers/NewPostRouter.tsx
@@ -5,6 +5,8 @@ import { MouseEvent } from 'react';
 import { MdOutlineDone } from 'react-icons/md';
 import { ClipLoader } from 'react-spinners';
 import Button from '../components/buttons/Button.tsx';
+import TextInput from '../components/inputs/TextInput.tsx';
+import TextAreaInput from '../components/inputs/TextAreaInput.tsx';
 
 
 
@@ -31,10 +33,10 @@ export default function NewPostRouter() {
 
     return (
         <form className='flex flex-col p-3' action="" onSubmit={e => e.preventDefault()}>
-            <label htmlFor="title">Title</label>
-            <input type="text" id="title" required onChange={e => setNewPost({ ...newPost, title: e.target.value })} autoFocus className="border border-black border-solid rounded-lg dark:bg-dark-blue " value={newPost.title} />
-            <label htmlFor="post">Post</label>
-            <textarea id="post" required rows={9} cols={10} onChange={e => setNewPost({ ...newPost, body: e.target.value })} className="border border-black border-solid rounded-lg mb-3 dark:bg-dark-blue" value={newPost.body} />
+            <label htmlFor="title">Title:</label>
+            <TextInput id='title' required onChange={e => setNewPost({ ...newPost, title: e.target.value })} value={newPost.title}/>
+            <label htmlFor="post">Post:</label>
+            <TextAreaInput id='post' required rows={9} cols={10} onChange={e => setNewPost({ ...newPost, body: e.target.value })} value={newPost.body}/>
             <Button onClick={e => handleSubmit(e, newPost!)}>
                 {isLoading ? <ClipLoader size={25} /> : isDone ?
                     <MdOutlineDone size={25} /> : "Submit"}
