628bfee692bf1d27677e63b3468c94899c420f5a
Halil Uzer <haliluzer@outlook.com>
2024-07-07 19:40:39 +0300

    blog: signUpRouter updated

--- blog/src/routers/SignUpRouter.tsx
+++ blog/src/routers/SignUpRouter.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { FaTimes, FaCheck } from "react-icons/fa";
 import Input from './../components/inputs/Input.tsx'
 import Button from '../components/buttons/Button.tsx'
 
@@ -7,7 +8,7 @@ const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
 export default function SignUpRouter() {
-    const userRef = useRef(null)
+    const userRef = useRef<HTMLInputElement>(null)
     const errRef = useRef(null)
 
     const [username, setUsername] = useState('')
@@ -24,6 +25,8 @@ export default function SignUpRouter() {
 
     const [errMsg, setErrMsg] = useState('')
     const [success, setSuccess] = useState(false)
+
+    userRef.current?.focus()
 
     useEffect(() => {
         setValidUsername(USER_REGEX.test(username))
@@ -43,11 +46,14 @@ export default function SignUpRouter() {
 
     return (
         <main className='flex justify-center items-center min-h-[100vh] w-full max-w-[300px] dark:black dark:text-white m-auto'>
-            <h1 className='mr-auto font-bold text-lg'>Sign-Up</h1>
-            <p ref={errRef}>{errMsg}</p>
             <form className='flex flex-col justify-center items-center p-5 bg-light-blue dark:bg-gray-600 w-full' action="">
-                <label htmlFor="username" className='mr-auto'>Username:</label>
-                <Input id='username' required autoComplete='off' value={username} ref={userRef} onChange={e => setUsername(e.target.value)} />
+                <h1 className='mr-auto font-bold text-lg'>Sign-Up</h1>
+                <p ref={errRef}>{errMsg}</p>
+                <label htmlFor="username" className='mr-auto'>
+                    Username: {username.length === 0 ? null : validUsername ?
+                        <FaCheck className='text-green-600 inline' /> : <FaTimes className='text-red-600 inline' />}
+                </label>
+                <Input id='username' required autoComplete='off' value={username} ref={userRef} onChange={e => setUsername(e.target.value)} autoFocus />
                 <label htmlFor="password" className='mr-auto'>Password:</label>
                 <Input id='password' type='password' required value={pwd} onChange={e => setPwd(e.target.value)} />
                 <label htmlFor="confirm_pwd" className='mr-auto'>Confirm Password:</label>
