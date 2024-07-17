fb83563bf0e476152121fae240e68dce80b3c2c2
Halil Uzer <haliluzer@outlook.com>
2024-07-17 15:07:30 +0300

    blog: useAxios hook updated

--- blog/src/hooks/useAxios.ts
+++ blog/src/hooks/useAxios.ts
@@ -1,5 +1,36 @@
+import { useEffect, useState } from "react";
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios"
 
-const useAxios = (configObj) => {
+const useAxios = (configObj: AxiosRequestConfig) => {
+
+    const [response, setResponse] = useState([]);
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(true);
+    const [reload, setReload] = useState(0);
+
+    const refetch = () => setReload(prev => prev + 1)
+
+    useEffect(() => {
+        const controller = new AbortController();
+
+        const fetchData = async () => {
+            try {
+                const response: AxiosResponse = await axios({ ...configObj, signal: controller.signal })
+                setResponse(response.data)
+            } catch (err: any) {
+                console.log(err.message);
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchData();
+
+        return () => controller.abort();
+    }, [reload]);
+
+    return [response, error, loading, refetch]
 
 }
 
